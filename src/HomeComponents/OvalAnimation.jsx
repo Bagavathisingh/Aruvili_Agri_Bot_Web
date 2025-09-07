90952325bb71d5f8487be1af7949d21665a882e6
bagavathisingh <bugzx25@gmail.com>
2025-09-07 22:00:39 +0530

    add some designs

--- src/HomeComponents/OvalAnimation.jsx
+++ src/HomeComponents/OvalAnimation.jsx
@@ -12,56 +12,29 @@ export default function Oval() {
          y: 0, opacity: 1
          }}
         transition={{ 
-            duration: 2, ease: "easeOut"
+            duration: 6,
+            type:"spring"
          }}
         className="absolute h-40 w-35 bg-green-300 rounded-full left-30 rotate-45 bottom-20"
       >
-        {/* Nested motion for infinite floating */}
-        <motion.div
-          animate={{ y: [0, -40, 0, 40, 0] }}
-          transition={{
-            duration: 6,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-          className="h-full w-full"
-        />
       </motion.div>
 
       {/* Right oval */}
       <motion.div
         initial={{ y: 500, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 2.5, ease: "easeOut" }}
+        transition={{ duration: 6, type:"spring"}}
         className="absolute h-40 w-35 bg-green-300 rounded-full right-30 -rotate-45 top-20"
       >
-        <motion.div
-          animate={{ y: [0, -40, 0, 40, 0] }}
-          transition={{
-            duration: 7,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-          className="h-full w-full"
-        />
       </motion.div>
 
       {/* Small circle */}
       <motion.div
         initial={{ y: 500, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 3, ease: "easeOut" }}
+        transition={{ duration: 5, type:"spring" }}
         className="h-10 w-10 absolute bottom-4 bg-green-700 rounded-full right-30"
       >
-        <motion.div
-          animate={{ y: [0, -30, 0, 30, 0], rotate: [0, 36, 0] }}
-          transition={{
-            duration: 5,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-          className="h-full w-full"
-        />
       </motion.div>
     </>
   );
--- src/MainPages/MainPageContent.jsx
+++ src/MainPages/MainPageContent.jsx
@@ -1,9 +1,114 @@
+import { easeInOut, motion } from "framer-motion";
+import { feDropShadow } from "framer-motion/m";
+
 export default function MainPageContent (){
     return (
         <>
-        <div className="h-full w-300 -ml-30 z-1 backdrop-blur-xl">
-            <div className="">
+        <div className="h-full w-300 md:absolute -ml-30">
+            <div className="h-40 mt-10 flex flex-col justify-center items-center">
+                <h1 className="text-5xl inter flex gap-5 justify-center items-center">
+                    <motion.div
+                    initial={{
+                        y:-200,
+                        opacity:0,
+                        rotate:120
+                    }}
+                    animate={{
+                        y:0,
+                        opacity:1,
+                        rotate:6
+                    }}
+                    transition={{
+                        duration:2,
+                        type:"spring"
+                    }}
+                     className="back p-3 rounded-lg rotate-6">
+                        <span className="inter">Benfits</span>
+                    </motion.div>
+                    <motion.span
+                    initial={
+                        {
+                            opacity:0,
+                            scale:0
+                        }
+                    }
+                    transition={{
+                        duration:2,
+                        type:"spring"
+                    }}
+                    animate={{
+                        opacity:1,
+                        scale:1
+                    }}
+                    >Of Our Services</motion.span></h1>
+            </div>
+            <div className="h-100 flex">
+                <div className="w-full flex justify-center items-center p-1">
 
+
+
+                    <motion.div 
+                    initial={{
+                        opacity:0.9,
+                        scale:0.5
+                    }}
+                    animate={{
+                        scale:1,
+                        opacity:1
+                    }}
+                    transition={{
+                        duration:2,
+                        type:"string"
+                    }}
+                    className="drop-shadow-xl drop-shadow-black rounded-2xl flex gap-5 p-1 bg-green-300 h-70 w-full">
+                        <motion.div 
+                        initial={{
+                            y:800,
+                            rotate:120
+                        }}
+                        animate={{
+                            y:0,
+                            rotate:-5
+                        }}
+                        transition={{
+                            duration:4,
+                            type:"spring"
+                        }}
+                        className="w-65 shadow-2xl shadow-black opacity-100 rounded-2xl back -rotate-6">
+
+                        </motion.div>
+                        <div className="w-125 gap-10 rounded-2xl flex flex-col ">
+                            <div className="rounded-2xl p-1 mt-2 flex-col gap-5 flex justify-center">
+                                <h1 className="text-xl">Make Your Farm 100%<span className="ml-1 p-1 rounded-lg bg-green-500 font-bold">Organic & Full Automated</span></h1>
+                                <div className="mt-2 h-20 rounded-lg">
+                                    <h2 className="">We Are The Part Of Your Farm And Your Life To Organic And Healthy <span className="ml-20 ">Live The Life With Us And Our Service</span></h2>
+                                </div>
+                            </div>
+
+                            < div className="p-5 rounded-2xl h-20 flex justify-center items-center">
+                                <motion.button
+                                whileHover={
+                                    {
+                                        scale:1.1,
+                                        boxShadow:"1px 1px 10px black",
+                                        transition:{
+                                            duration:0.30,
+                                            ease:easeInOut
+                                        }
+                                    }
+                                }
+                                 className="bg-green-600 hover:bg-green-700 hover:text-white inter-normal px-10 py-1 rounded-lg
+                                transition duration-300 ease-in-out ">Learn More</motion.button>
+                            </div>
+                        </div>
+                    </motion.div>
+
+
+
+                </div>
+                <div className="border w-150">
+
+                </div>
             </div>
         </div>
         </>
